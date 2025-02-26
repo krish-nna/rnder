45eba2dab3a9f492a09735d5f2938a5e03c87b47
krish-nna <krushnabadgujarr@gmail.com>
2025-02-26 19:48:04 +0530

    modified remaining files

--- fetch_students.php
+++ fetch_students.php
@@ -1,7 +1,8 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+hheader("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 
 // Enable error reporting for debugging
 error_reporting(E_ALL);
