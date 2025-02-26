b84b77ad8dc4029278fe35b7a0fe62e6b4c152fc
krish-nna <krushnabadgujarr@gmail.com>
2025-02-26 22:31:34 +0530

    modified remaining files

--- delete_students.php
+++ delete_students.php
@@ -1,4 +1,5 @@
-<?phpheader("Access-Control-Allow-Origin: *");
+<?php
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
